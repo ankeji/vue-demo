790eff49f76e1ee139bdc987ce6ac3e1889f6cd7
ankeji <ankeji1995@163.com>
2020-10-20 10:50:36 +0800

    优化config配置

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,16 @@
  * @Author: ankeji
  * @Date: 2020-07-06 18:21:11
  * @LastEditors: ankeji
- * @LastEditTime: 2020-07-06 19:31:15
+ * @LastEditTime: 2020-10-20 10:50:06
  */
 const path = require('path')
-
+console.log(process.env.npm_config_report, '安科吉');
 // 拼接路径
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 const CompressionPlugin = require('compression-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
     publicPath: '/', // 默认为'/'
 
@@ -41,51 +41,66 @@ module.exports = {
             .set('assets', resolve('src/assets'))
             .set('components', resolve('src/components'))
             .set('views', resolve('src/views'))
-     /**
-     * 删除懒加载模块的 prefetch preload，降低带宽压力
-     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
-     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
-     * 而且预渲染时生成的 prefetch 标签是 modern 版本的，低版本浏览器是不需要的
-     */
+        /**
+        * 删除懒加载模块的 prefetch preload，降低带宽压力
+        * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
+        * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
+        * 而且预渲染时生成的 prefetch 标签是 modern 版本的，低版本浏览器是不需要的
+        */
         config.plugins
             .delete('prefetch')
             .delete('preload')
         // 解决 cli3 热更新失效 https://github.com/vuejs/vue-cli/issues/1559
         config.resolve
-            .symlinks(true)  
+            .symlinks(true)
         if (process.env.NODE_ENV === 'production') {
             config.optimization.minimizer('terser').tap((args) => {
                 // 去除生产环境console
                 args[0].terserOptions.compress.drop_console = true
                 return args
             })
-         /**
-         * 生产环境用远程cdn包缩小项目体积，优化首页加载速度
-         */
-          config.externals({
+            /**
+            * 生产环境用远程cdn包缩小项目体积，优化首页加载速度
+            */
+            config.externals({
                 'vue': 'Vue',
                 'vuex': 'Vuex',
                 'vue-router': 'VueRouter',
                 'axios': 'axios'
             })
-        }  
+        }
     },
     configureWebpack: (config) => {
         if (process.env.NODE_ENV === 'production') {
+            if (process.env.npm_config_report) {
+                config.plugins.push(
+                    new BundleAnalyzerPlugin({
+                        analyzerMode: 'server',
+                        analyzerHost: '127.0.0.1',
+                        analyzerPort: 8889,
+                        reportFilename: 'report.html',
+                        defaultSizes: 'parsed',
+                        openAnalyzer: true,
+                        generateStatsFile: false,
+                        statsFilename: 'stats.json',
+                        statsOptions: null,
+                        logLevel: 'info'
+                    }))
+            }
             return {
                 plugins: [
                     new CompressionPlugin({
                         test: /\.js$|\.html$|.\css/, //匹配文件名
                         threshold: 10240, //对超过10k的数据压缩
                         deleteOriginalAssets: false //不删除源文件
-                    }),
-                    new BundleAnalyzerPlugin()
+                    })
                 ],
                 output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
                     filename: `./assets/js/[name].[chunkhash].js`, //不加版本号
                     chunkFilename: `./assets/js/[name].[chunkhash].js`
-                },
+                }
             }
+
         }
     },
     lintOnSave: false,
@@ -109,6 +124,6 @@ module.exports = {
             },
         },
     },
-     // 构建时开启多进程处理 babel 编译
+    // 构建时开启多进程处理 babel 编译
     parallel: require('os').cpus().length > 1,
 }
