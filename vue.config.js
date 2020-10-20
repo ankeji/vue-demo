31b6528e5b25cd807d9bf3b2981bed2e93968ebe
ankeji <48073752+ankeji@users.noreply.github.com>
2020-10-20 09:40:48 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -41,14 +41,27 @@ module.exports = {
             .set('assets', resolve('src/assets'))
             .set('components', resolve('src/components'))
             .set('views', resolve('src/views'))
-            
+     /**
+     * 删除懒加载模块的 prefetch preload，降低带宽压力
+     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
+     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
+     * 而且预渲染时生成的 prefetch 标签是 modern 版本的，低版本浏览器是不需要的
+     */
+        config.plugins
+            .delete('prefetch')
+            .delete('preload')
+        // 解决 cli3 热更新失效 https://github.com/vuejs/vue-cli/issues/1559
+        config.resolve
+            .symlinks(true)  
         if (process.env.NODE_ENV === 'production') {
             config.optimization.minimizer('terser').tap((args) => {
                 // 去除生产环境console
                 args[0].terserOptions.compress.drop_console = true
                 return args
             })
-    
+         /**
+         * 生产环境用远程cdn包缩小项目体积，优化首页加载速度
+         */
           config.externals({
                 'vue': 'Vue',
                 'vuex': 'Vuex',
@@ -59,15 +72,20 @@ module.exports = {
     },
     configureWebpack: (config) => {
         if (process.env.NODE_ENV === 'production') {
-            config.plugins.push(
-                new CompressionPlugin({
-                    // gzip压缩配置
-                    test: /\.js$|\.html$|\.css/, // 匹配文件名
-                    threshold: 10240, // 对超过10kb的数据进行压缩
-                    deleteOriginalAssets: false, // 是否删除原文件
-                }),
-                new BundleAnalyzerPlugin()
-            )
+            return {
+                plugins: [
+                    new CompressionPlugin({
+                        test: /\.js$|\.html$|.\css/, //匹配文件名
+                        threshold: 10240, //对超过10k的数据压缩
+                        deleteOriginalAssets: false //不删除源文件
+                    }),
+                    new BundleAnalyzerPlugin()
+                ],
+                output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+                    filename: `./assets/js/[name].[chunkhash].js`, //不加版本号
+                    chunkFilename: `./assets/js/[name].[chunkhash].js`
+                },
+            }
         }
     },
     lintOnSave: false,
@@ -90,5 +108,7 @@ module.exports = {
                 },
             },
         },
-    }
+    },
+     // 构建时开启多进程处理 babel 编译
+    parallel: require('os').cpus().length > 1,
 }
