18eaf3d91ada77421fd77389be79b8aa0f18ab7f
ankeji <ankeji1995@163.com>
2021-03-03 11:52:55 +0800

    空提交

--- src/api/modules/userApi.js
+++ src/api/modules/userApi.js
@@ -4,7 +4,7 @@
  * @Author: ankeji
  * @Date: 2020-06-09 16:06:11
  * @LastEditors: ankeji
- * @LastEditTime: 2020-07-06 18:01:37
+ * @LastEditTime: 2021-03-03 11:52:41
  */ 
 import method from "../service"
 const login = {}
@@ -13,7 +13,7 @@ const login = {}
 /**
  * @name 手机号登录
  * @username 用户名
- * @password 密码
+ * @password 密 码
  */
 login.iphone_login_in = (param)=>{
     return method.nopostJson(`http://172.0.0.0:8088/services/authentication/api/authenticate?provider=otp`, param)
